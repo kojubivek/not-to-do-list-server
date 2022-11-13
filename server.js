e4cc8333129bcb6dfca6d8b056e3243daa5e2753
koju <kojubivek@gmail.com>
2022-11-14 09:29:58 +1100

    addded documentations

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 
 const app = express();
+const PORT = 8000;
 //middleware
 app.use(morgan("dev"));
 
@@ -15,21 +16,19 @@ app.post("/api/v1/task", (req, res) => {
 });
 //READ: read data from DATABASE and return to the client
 app.get("/api/v1/task", (req, res) => {
-  res.json({ message: "tod get method" });
+  res.json({ message: "tod get get" });
 });
 //update: update some information of existing data in the database and respond client accordingly
 
 app.put("/api/v1/task", (req, res) => {
-  res.json({ message: "tod get method" });
+  res.json({ message: "tod get put" });
 });
 //D(delete): delete data(s) from database and response clent accordingly
 app.delete("/api/v1/task", (req, res) => {
-  res.json({ message: "tod get method" });
+  res.json({ message: "tod get delete" });
 });
 
 console.log(app);
-
-const PORT = 8000;
 
 app.listen(PORT, (error) => {
   error
